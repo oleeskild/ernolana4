3001b52c4632d9cc69b764d9d935626dd2d3c13c
Ole Eskild Steensen <ole.steensen@knowit.no>
2020-09-08 15:46:42 +0200

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2> Er Nolan A4?</h2>
+
+<transition name="fade">
+    <h1 v-if="show"> NEI STIAN.</h1>
+</transition>
+<transition name="fade">
+    <h1 v-if="show2">NEI!</h1>
+</transition>
+<transition name="fade">
+    <h1 v-if="show3">Stian... NEI</h1>
+</transition>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data(){
+    return {
+      show: false,
+      show2: false,
+      show3: false
+    }
+  },
+  mounted: function(){
+    this.show=true;
+    setTimeout((()=>this.show2=true), 1000)
+    setTimeout((()=>this.show3=true), 3000)
   }
 }
 </script>
@@ -24,5 +43,24 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  font-size: 92px;
+}
+h2 {
+  font-size: 64px;
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 3s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+
+.fade2-enter-active, .fade-leave-active {
+  transition: opacity 3s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
